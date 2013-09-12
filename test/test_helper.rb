03aacdd777dfb52584738fdd62f4fa2cb83d49c4
DavidRagone <david@ouya.tv>
2013-09-11 23:40:55 -0700

    Move request_methods logic to helper, make global

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,33 @@ require 'rest-client'
 require 'json'
 
 alias :context :describe
+
+$request_methods = ->(method, request_method) do
+  describe "##{method}" do
+    before { @model = @class.new }
+
+    it "requests #{method.to_s.gsub('_', ' ')}" do
+      @mock_requester = MiniTest::Mock.new
+      @mock_requester.expect request_method, :return_value, [@model.id]
+      @model.stub(:request, @mock_requester) do
+        @model.send(method)
+        @mock_requester.verify
+      end
+    end
+
+    it "memoizes" do
+      @mock_requester = MiniTest::Mock.new
+      @mock_requester.expect request_method, :return_value, [@model.id]
+      @model.stub(:request, @mock_requester) do
+        @model.send(method)
+        @mock_requester.verify
+        @model.instance_variables.must_include "@#{method}".to_sym
+        @mock_requester.expect request_method, true
+        @model.send(method)
+        assert_raises(MockExpectationError, "#{request_method} should not be called (memoized)") do
+          @mock_requester.verify
+        end
+      end
+    end
+  end
+end
